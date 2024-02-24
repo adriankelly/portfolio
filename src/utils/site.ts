c915d819ff17cac076b449c7027876fa8729f076
Adrian Kelly <a.kelly7@gmail.com>
2024-02-24 22:20:28 +0100

    page title

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -4,7 +4,7 @@
  * @param siteTitle
  * @returns
  */
-export function convertPageTitle(pageTitle: string, siteTitle: string): string {
-	let titleSeparator = " - ";
-	return pageTitle + titleSeparator + siteTitle;
-}
+// export function convertPageTitle(pageTitle: string, siteTitle: string): string {
+// 	let titleSeparator = " - ";
+// 	return pageTitle + titleSeparator + siteTitle;
+// }
