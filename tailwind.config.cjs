675616531888f5875594bbd27b9f6c9c2b7cc2c8
Adrian Kelly <a.kelly7@gmail.com>
2024-02-24 06:03:31 +0100

    add pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,15 @@ module.exports = {
 				/** Change here the font family */
 				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
 			},
+			fontSize: {
+				sm: '0.8rem',
+				base: '1rem',
+				xl: '1.25rem',
+				'2xl': '1.563rem',
+				'3xl': '1.953rem',
+				'4xl': '2.441rem',
+				'5xl': '3.052rem',
+			},
 			colors: {
 				/** Override here your primary colors */
 				primary: {
